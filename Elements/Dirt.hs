dbf190561e53907876b5473c9bc6fc843f4ea3d7
Abelo98 <abelo08cruz@gmail.com>
2022-02-04 03:09:55 -0500

    now the grid selected is check and it takes in count the central position to

--- Elements/Dirt.hs
+++ Elements/Dirt.hs
@@ -10,9 +10,9 @@ import System.Random (StdGen, newStdGen)
 generateDirt :: Int -> [(Int,Int)] -> StdGen -> StdGen-> [(Int,Int)]
 generateDirt _ [] _ _ = [] 
 generateDirt kidsAround ady genToPick genToMess 
-    | kidsAround == 0 = let mess = head (randomNumbers 2 genToMess)
+    | kidsAround == 1 = let mess = head (randomNumbers 2 genToMess)
                         in pickRandom ady mess genToPick
-    | kidsAround == 1 =  let mess = head (randomNumbers 4 genToMess)
+    | kidsAround == 2 =  let mess = head (randomNumbers 4 genToMess)
                         in pickRandom ady mess genToPick
     | otherwise = let mess = head (randomNumbers 7 genToMess)
                   in pickRandom ady mess genToPick
--- Elements/Kids.hs
+++ Elements/Kids.hs
@@ -16,7 +16,7 @@ moveKids env gen =
     let binarySec = randomNumbers 2 gen
         selected_kids = selection binarySec (length (kids env))
         in simulateMoves selected_kids (kids env) gen env
- 
+
 selection _ 0 = []
 selection (x:xs) n = x:selection xs (n-1)
 
@@ -25,8 +25,8 @@ findMoves pos env =
     let mov = filterCells emptyCell (getAdy pos env) env pos
     in if null mov then [pos] else mov
 
-scanKids :: (Int, Int) -> ENV -> Int
-scanKids pos env = countKids (getAdy pos env) env
+scanKids :: [(Int, Int)] -> ENV -> Int
+scanKids = countKids
 
 carriedKids = length . filter id
 
@@ -35,8 +35,8 @@ computeKidsInGrid pos env gen =
         cells_in_my_grid = getAdy pos env ++ [pos]
         (i,_) = randomR (0,(length cells_in_my_grid-1)::Int) gen
         center_cell_to_scan = cells_in_my_grid!!i
-        new_grid = getAdy center_cell_to_scan env
-    in (scanKids center_cell_to_scan env, new_grid)
+        new_grid = getAdy center_cell_to_scan env ++ [center_cell_to_scan]
+    in (scanKids new_grid env , new_grid)
 
 
 makeMove o d env = remove o (kids env) ++ [d]
@@ -57,7 +57,7 @@ simulateMoves (1:xs) (o:restKidsPos) gen env =
             let poss_dirty_cells = emptyCellToMess grid2 env
                 (_,gen2) = randomR (0,2::Int) gen
                 (_,gen3) = randomR (0,3::Int) gen
-                new_dirt = dirty env ++ generateDirt (scanKids o env) poss_dirty_cells gen2 gen3
+                new_dirt = dirty env ++ generateDirt kids_in_grid poss_dirty_cells gen2 gen3
             in if inList dest (obstc env)
                 then
                 let newObstc = moveObstc dest (getDir dest o) (obstc env)
