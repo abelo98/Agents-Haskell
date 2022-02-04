03973b7c0d7e4297f930ab6c9b9e14287ef77627
Abelo98 <abelo08cruz@gmail.com>
2022-02-04 02:45:09 -0500

    change cells to generate dirt

--- Elements/Kids.hs
+++ Elements/Kids.hs
@@ -84,11 +84,11 @@ simulateMoves2 (1:xs) (o:restKidsPos) gen env =
      let (_,gen1) = randomR (0,1::Int) gen
          moves = findMoves2 o env
          dest = head (pickRandom moves 1 gen1)
-         kids_in_grid = scanKids o env 
-         grid = remove dest (getAdy o env)
+         (kids_in_grid,grid) = computeKidsInGrid o env gen1
+         grid2 = remove dest grid
          new_kids = makeMove o dest env
         in
-            let poss_dirty_cells = emptyCellToMess grid env
+            let poss_dirty_cells = emptyCellToMess grid2 env
                 (_,gen2) = randomR (0,2::Int) gen
                 (_,gen3) = randomR (0,3::Int) gen
                 new_dirt = dirty env ++ generateDirt (scanKids o env) poss_dirty_cells gen2 gen3
@@ -106,7 +106,7 @@ simulateMoves2 (1:xs) (o:restKidsPos) gen env =
 computeKidsInGrid pos env gen =
     let
         cells_in_my_grid = getAdy pos env ++ [pos]
-        (i,_) = randomR (0,length cells_in_my_grid::Int) gen
+        (i,_) = randomR (0,(length cells_in_my_grid-1)::Int) gen
         center_cell_to_scan = cells_in_my_grid!!i
         new_grid = getAdy center_cell_to_scan env
     in (scanKids center_cell_to_scan env, new_grid)
